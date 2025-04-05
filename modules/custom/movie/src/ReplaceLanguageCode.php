f8ad2cd1a1209d002c26d96adf560be177b56d83
ankit5 <vermankit5@gmail.com>
2025-04-05 12:24:57 +0530

    sdf

--- modules/custom/movie/src/ReplaceLanguageCode.php
+++ modules/custom/movie/src/ReplaceLanguageCode.php
@@ -306,7 +306,8 @@ function curlgetmoviebox($i,$channel_id){
   curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
   curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0");
   curl_setopt($curl, CURLOPT_HTTPHEADER , array(
-    'Referer: https://h5.inmoviebox.com/',
+    'Referer: https://h5.inmoviebox.com/web/filter?tab=movie',
+    'Origin: https://h5.inmoviebox.com'
   ));
   $str = curl_exec($curl);
   curl_close($curl);
