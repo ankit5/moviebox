fed5b2850c81fdc243e011a11e6e643973dfea04
ankit5 <vermankit5@gmail.com>
2025-05-18 12:38:24 +0530

    srs

--- modules/custom/movie/src/ReplaceLanguageCode.php
+++ modules/custom/movie/src/ReplaceLanguageCode.php
@@ -104,7 +104,7 @@ public static function replaceLangcode3($nid, &$context){
     $node = \Drupal::entityTypeManager()->getStorage('node')->load($nid);
     $message = 'Replacing langcode(und to de)...';
     $load ='';
-    if($node->field_season->value=='' && $node->field_subjecttype->value=='1')
+    if($node->field_season->value=='' && $node->field_subjecttype->value=='2')
     {
       $load =1;
     } 
@@ -112,7 +112,7 @@ public static function replaceLangcode3($nid, &$context){
 //  print $node->field_url->value;
 // var_export($node->field_subjectid->value);
 //  exit;
-   $message2 = getmoviebox_detail($node->field_detailpath->value,$node->field_subjectid->value);
+   $message2 = getmoviebox_detail_session($node->field_detailpath->value,$node->field_subjectid->value);
  
 // print_r($message2);
  
@@ -526,6 +526,69 @@ $text = $block->field_movie->getValue();
       $block->field_movie = $output;
        $results[] = $block->save();
 }
+
+function getmoviebox_detail_session($detailpath='',$subjectid='')
+{
+  
+  $curl = curl_init();
+  $url = 'https://api6.aoneroom.com/wefeed-mobile-bff/subject-api/season-info?subjectId='.$subjectid;
+ 
+ curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+curl_setopt($curl, CURLOPT_HEADER, false);
+curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+//curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0");
+ curl_setopt($curl, CURLOPT_HTTPHEADER , array(
+    'Referer: https://api6.aoneroom.com',
+    'Origin: https://api6.aoneroom.com',
+    'authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjMzNzA3MTI1MDUwNTkzNjMwMDgsImV4cCI6MTc1MjE0NDI1MCwiaWF0IjoxNzQ0MzY3OTUwfQ.sedWQ7HL-5WPOqWceQXoR4fnaGg6y3xmqT6GzcVCyGU',
+    'Host: api6.aoneroom.com',
+    'x-tr-signature: 1744448656252|2|G2yxmgowWJjgahvtpBXArw==',
+    'user-agent:: com.community.oneroom/50020038 (Linux; U; Android 7.1.2; hi_IN; SM-N976N; Build/QP1A.190711.020; Cronet/136.0.7064.0)',
+    'x-client-info: {"package_name":"com.community.oneroom","version_name":"3.0.01.0411.03","version_code":50020038,"os":"android","os_version":"7.1.2","install_ch":"ps","device_id":"ce2435d7e22e3fb3dc80710311df803a","install_store":"ps","gaid":"ddf9ce6c-fed8-4704-abb4-d79915482cc7","brand":"samsung","model":"SM-N976N","system_language":"hi","net":"NETWORK_WIFI","region":"IN","timezone":"Asia/Calcutta","sp_code":"40416","X-Play-Mode":"2"}'
+    // 'X-Forwarded-For: http://localhost'
+  ));
+$str = curl_exec($curl);
+curl_close($curl);
+//var_export($str);
+
+// var_export($str);
+//  exit;
+
+$str_new = json_decode($str);
+// print "<pre>";
+//  print_r($str_new);
+// exit;
+ $season_id = '';
+ if(@$str_new->data->seasons[0]->se) $season_id = $str_new->data->seasons[0]->se;
+
+if($season_id==''){
+  return true;
+}
+
+
+//print_r($str_new[22]);
+$season = [];
+foreach($str_new->data->seasons as $key=>$value){
+ // print_r($str_new[$value]->se);
+  $season[$key]['se']=$value->se;
+  $season[$key]['ep']=$value->maxEp;
+}
+$season = json_encode($season);
+// print "<pre>";
+//  print_r($str_new);
+// print "<pre>";
+//  print_r($season);
+//   exit;
+ 
+  $movie['field_season'] = $season;
+
+
+    return $movie;
+}
+
+
 
 function getmoviebox_detail($detailpath='',$subjectid='')
 {
