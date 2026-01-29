1662bda428283d4692c40e2cca6a45300b4f4a41
Ankit Verma <vermankit5@gmail.com>
2026-01-29 14:59:09 +0530

    Update ReplaceLanguageCode.php

--- modules/custom/movie/src/ReplaceLanguageCode.php
+++ modules/custom/movie/src/ReplaceLanguageCode.php
@@ -105,6 +105,8 @@ public static function replaceLangcode3($nid, &$context){
     $message = 'Replacing langcode(und to de)...';
     $results = array();
      // $message2 = getmoviebox_detail_session_old($node->field_detailpath->value,$node->field_subjectid->value);
+    print $node->field_loaded->value;
+    
      if($node->field_subjectid->value=='8802963787692345120old'){
    $message3 =  getmoviebox_detail_dub_trailer($node->field_detailpath->value,$node->field_subjectid->value);
     
